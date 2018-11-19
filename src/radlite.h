8a2db59fad6b1a8010065c4e8a095f6e2e1df498
Ty Pauly <tpauly@snowline.stsci.edu>
2018-11-19 09:31:18 -0500

    Pushed filenames from main to radlite.h

--- src/radlite.h
+++ src/radlite.h
@@ -1,6 +1,19 @@
-#define rlen 131
-#define tlen 80
-#define dencol 3
+#define RLEN 131
+#define RADFILE "radius.dat"
+
+#define TLEN 80
+#define THTFILE "theta.dat"
+
+#define GRIDFILE "radlite_griddata_posval.dat"
+#define NCOL 13
+
+#define DENCOL 3
+#define ABNCOL 4
+#define TEMCOL 5
+#define DTEMCOL 6
+#define TURBCOL 7
+#define HCOL 11
+#define H2COL 12
 
 void gridread(double*** array, int* glen, double** thtvec, double** radvec);
 double bilinInterpVal(double x, double y, double z, int col);
